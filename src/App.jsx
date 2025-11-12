9bf25894800845dc977de078ed9f370ce4473120
Spandan-Bhattarai <sbhattarai50@gmail.com>
2025-11-12 10:26:35 +0000

    Adding route for landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Loginpage from "./pages/auth/Loginpage";
 import Registerpage from './pages/auth/Registerpage';
 import Feedpage from './pages/auth/Feedpage';
+import Dashboard from './pages/auth/Dashboard';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
           <Route path="/login" element={<Loginpage />} />
           <Route path="/register" element={<Registerpage />} />
           <Route path="/feed" element={<Feedpage />} />
+          <Route path="/" element={<Dashboard />} />
         </Routes>
     </>
   )
