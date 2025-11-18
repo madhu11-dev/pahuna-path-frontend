2196cae7b1eeb4e366081073e1e9b8d191619221
Spandan-Bhattarai <sbhattarai50@gmail.com>
2025-11-18 09:03:02 +0000

    Minor route fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,9 @@ import Loginpage from "./pages/auth/Loginpage";
 import Registerpage from "./pages/auth/Registerpage";
 import Dashboard from "./pages/Dashboard";
 import Feedpage from "./pages/user/Feedpage";
-<<<<<<< Updated upstream
 import AdminRoutes from "./protected_routes/AdminRoutes";
 import UserRoutes from "./protected_routes/UserRoutes";
-=======
 import Accomodations from "./pages/user/Accomodations";
->>>>>>> Stashed changes
 
 const App = () => {
   return (
@@ -17,7 +14,6 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Loginpage />} />
         <Route path="/register" element={<Registerpage />} />
-<<<<<<< Updated upstream
 
         <Route element={<AdminRoutes />}>
           <Route path="/admin" element={<AdminDashboard />} />
@@ -25,14 +21,9 @@ const App = () => {
 
         <Route element={<UserRoutes />}>
           <Route path="/feed" element={<Feedpage />} />
+          <Route path="/accomodate" element={<Accomodations/>} />
           <Route path="/" element={<Dashboard />} />
         </Route>
-=======
-        <Route path="/feed" element={<Feedpage />} />
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/accomodate" element={<Accomodations/>} />
->>>>>>> Stashed changes
       </Routes>
     </>
   );
