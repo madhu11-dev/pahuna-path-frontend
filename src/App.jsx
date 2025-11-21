4eae92e0a7624a44c0f84acd6bfca89cab7e0101
Suvani Basnet <suvanibasnet8@gmail.com>
2025-11-21 08:18:14 +0000

    Modified routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import { Route, Routes } from "react-router-dom";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
 import Loginpage from "./pages/auth/Loginpage";
 import Registerpage from "./pages/auth/Registerpage";
-import Dashboard from "./pages/Dashboard";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import ResetPassword from "./pages/auth/ResetPassword";
+import Landing from "./pages/Landing";
 import Feedpage from "./pages/user/Feedpage";
+import Accommodations from "./pages/user/Accommodations";
+import Explore from "./pages/user/Explore";
 import AdminRoutes from "./protected_routes/AdminRoutes";
 import UserRoutes from "./protected_routes/UserRoutes";
-// import Accomodations from "./pages/user/Accomodations";
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Loginpage />} />
         <Route path="/register" element={<Registerpage />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/admin" element={<AdminDashboard />} />
         <Route element={<AdminRoutes />}>
           {/* <Route path="/admin" element={<AdminDashboard />} /> */}
@@ -22,7 +27,8 @@ const App = () => {
 
         <Route element={<UserRoutes />}>
           <Route path="/feed" element={<Feedpage />} />
-          {/* <Route path="/accomodate" element={<Accomodations/>} /> */}
+          <Route path="/accommodations" element={<Accommodations />} />
+          <Route path="/explore" element={<Explore />} />
         </Route>
       </Routes>
     </>
