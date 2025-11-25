832e04f530896290886b583d160a50fa1c07265e
Risesh Shrestha <riseshstha222@gmail.com>
2025-11-25 06:48:48 +0000

    Navbar code refactored

--- src/components/user/UserNavbar.jsx
+++ src/components/user/UserNavbar.jsx
@@ -2,7 +2,7 @@ import { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LogOut } from 'lucide-react';
 import { toast } from 'react-toastify';
-import { logoutUserApi } from '../../apis/Api';
+import { logoutUserApi } from '../../apis/Api.js';
 
 const UserNavbar = () => {
     const navigate = useNavigate();
